68545da214e158721cd7d5eb4eee5f01989dbea2
lapshinfk <510002@mail.ru>
2021-04-27 22:28:43 +0400

    Пофиксил линки, чуть пофиксил верстку в целом, в "Phone reports" сделал новый компонент с табличкой с пагинацией и фильтрами

--- src/components/dashboard/ListItemButton.js
+++ src/components/dashboard/ListItemButton.js
@@ -6,7 +6,6 @@ import MenuItem from '@material-ui/core/MenuItem';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
 
 import {
     BrowserRouter as Router,
@@ -19,7 +18,7 @@ import {
 import './../styles/ListItemButton.scss';
 
 const ListItemButton = (props) => {
-    const { buttonName, tabMenuItems, isToolbarOpen, linkTo } = props;
+    const { buttonName, tabMenuItems, isToolbarOpen, linkTo, icon } = props;
 
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
@@ -31,41 +30,53 @@ const ListItemButton = (props) => {
     };
     return (
         <React.Fragment>
-            <ListItem button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                <ListItemIcon>
-                    <DashboardIcon />
-                </ListItemIcon>
-                {(linkTo &&
-                    <Link
-                        to={linkTo}
-                        style={{ color: 'rgba(0, 0, 0, 0.87)', textDecoration: 'none', fontSize: '16px', margin: '4px 0' }}>
-                        {buttonName}
-                    </Link>) ||
-                    <ListItemText primary={buttonName} />}
-            </ListItem>
-            {tabMenuItems && <Menu
-                PaperProps={{
-                    style: {
-                        marginLeft: isToolbarOpen ? '225px' : '57px'
-                    }
-                }}
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={open}
-                onClose={handleClose}
-            >
-                {tabMenuItems.map(element => {
-                    return <MenuItem onClick={handleClose}>
-                        <Link
-                            to={element.linkTo}
-                            style={{ color: 'rgba(0, 0, 0, 0.87)', textDecoration: 'none', fontSize: '16px', margin: '4px 0' }}>
-                            {element.text}
-                        </Link>
-                    </MenuItem>
-                })}
-            </Menu>}
-        </React.Fragment>
+            {(linkTo &&
+                <Link to={linkTo}
+                    style={{ color: 'rgba(0, 0, 0, 0.87)', textDecoration: 'none', fontSize: '16px', display: 'flex' }}>
+                    <ListItem button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                        <ListItemIcon>
+                            {icon}
+                        </ListItemIcon>
+                        <ListItemText primary={buttonName} />
+                    </ListItem>
+                </Link>
+            ) ||
+                <React.Fragment>
+                    <ListItem button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                        <ListItemIcon>
+                            {icon}
+                        </ListItemIcon>
+                        <ListItemText primary={buttonName} />
+                    </ListItem>
+                </React.Fragment>
+            }
+            {
+                tabMenuItems && <Menu
+                    PaperProps={{
+                        style: {
+                            marginLeft: isToolbarOpen ? '225px' : '57px'
+                        }
+                    }}
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={open}
+                    onClose={handleClose}
+                >
+                    {tabMenuItems.map(element => {
+                        return <MenuItem 
+                            style={{ padding: 0 }}
+                            onClick={handleClose}>
+                            <Link
+                                to={element.linkTo}
+                                style={{ color: 'rgba(0, 0, 0, 0.87)', textDecoration: 'none', fontSize: '16px', padding: '10px 15px', display: 'flex', width: '100%' }}>
+                                {element.text}
+                            </Link>
+                        </MenuItem>
+                    })}
+                </Menu>
+            }
+        </React.Fragment >
     )
 }
 
--- src/components/dashboard/listItems.js
+++ src/components/dashboard/listItems.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ListItemButton from './ListItemButton';
 
+import BusinessIcon from '@material-ui/icons/Business';
+import DevicesIcon from '@material-ui/icons/Devices';
+import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
+import DnsIcon from '@material-ui/icons/Dns';
+import ReportIcon from '@material-ui/icons/Report';
+import PhonelinkRingIcon from '@material-ui/icons/PhonelinkRing';
+import BuildIcon from '@material-ui/icons/Build';
+
 export const MainListItems = (props) => {
     const { isToolbarOpen } = props;
     return (
         <div>
-            <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="Офисы" linkTo="/empty-page">
+            <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="Офисы" linkTo="/empty-page" icon={<BusinessIcon />}>
             </ListItemButton>
-            <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="Оборудование" linkTo="/empty-page">
+            <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="Оборудование" linkTo="/empty-page" icon={<DevicesIcon />}>
             </ListItemButton>
             <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="Phones"
                 tabMenuItems={
@@ -16,28 +24,28 @@ export const MainListItems = (props) => {
                         { text: 'Телефоны по ФИО', linkTo: '/empty-page' },
                         { text: 'Телефоны с переадресацией', linkTo: '/empty-page' }
                     ]
-                }>
+                } icon={<PhoneIphoneIcon />}>
             </ListItemButton>
             <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="IP Planning"
                 tabMenuItems={
                     [
                         { text: 'IPAM', linkTo: '/ip-page' }
                     ]
-                }>
+                } icon={<DnsIcon />}>
             </ListItemButton>
-            <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="Reports" linkTo="/empty-page">
+            <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="Reports" linkTo="/empty-page" icon={<ReportIcon />}>
             </ListItemButton>
-            <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="Phone reports"
+            <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="Phone reports" icon={<PhonelinkRingIcon />}
                 tabMenuItems={
                     [
-                        { text: 'По моделям', linkTo: '/reports-page' },
-                        { text: 'По кластерам', linkTo: '/reports-page' },
-                        { text: 'По не используемым', linkTo: '/reports-page' },
-                        { text: 'По agent licenses', linkTo: '/reports-page' }
+                        { text: 'По моделям', linkTo: '/phones-page' },
+                        { text: 'По кластерам', linkTo: '/phones-page' },
+                        { text: 'По не используемым', linkTo: '/phones-page' },
+                        { text: 'По agent licenses', linkTo: '/phones-page' }
                     ]
                 }>
             </ListItemButton>
-            <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="Tools"
+            <ListItemButton isToolbarOpen={isToolbarOpen} buttonName="Tools" icon={<BuildIcon />}
                 tabMenuItems={
                     [
                         { text: 'Поиск незарегистрированных телефонов', linkTo: '/reports-page' },
